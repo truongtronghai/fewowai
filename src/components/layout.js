203b5e4cc283ab2a74695a632687aa7279bdb97e
Truong Trong Hai <truong.hai@del-qui.com>
2024-02-24 06:19:49 +0700

    first commit

---
+++ src/components/layout.js
@@ -0,0 +1,7 @@
+export default function Layout({ children }) {
+  return (
+    <>
+      <div className="max-w-md mx-auto p-4">{children}</div>
+    </>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,188 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useRef, useState, useEffect } from "react"
 
 export default function Home() {
+  const fakeUserId = 5
+  const urlGetTodos = `https://dummyjson.com/todos/user/${fakeUserId}`
+
+  const urlAddTodo = `https://dummyjson.com/todos/add`
+  const [cache, setCache] = useState([])
+  const [todoList, setTodoList] = useState(null)
+  const inputTitle = useRef(null)
+  const inputDesc = useRef(null)
+  const [isDisabled, setIsDisabled] = useState(true)
+
+  useEffect(() => {
+    const dataTodoList = localStorage.getItem("todos")
+    setIsDisabled(true)
+    if (dataTodoList === null) {
+      fetch(urlGetTodos)
+        .then((res) => res.json())
+        .then((data) => {
+          const todos = data.todos.map((item) => {
+            return { ...item, todo: item.todo + "|fake description" }
+          })
+          localStorage.setItem("todos", JSON.stringify(todos))
+
+          setCache(todos)
+        })
+    } else {
+      setCache(JSON.parse(dataTodoList))
+    }
+  }, [])
+
+  useEffect(() => {
+    console.log("Cache changed ===> ", cache)
+    localStorage.setItem("todos", JSON.stringify(cache))
+    updateTodoList(cache)
+  }, [cache])
+
+  const updateTodoList = (todos) => {
+    setTodoList(
+      todos.map((item) => {
+        return (
+          <li key={item.id} className="border-b-2 border-gray-200">
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                checked={item.completed}
+                className="mr-2"
+                onChange={() => handleChangeCheckbox(item.id)}
+              />
+              <div className="flex flex-col grow">
+                <input
+                  id={`title${item.id}`}
+                  className={`${item.completed ? "line-through" : ""}`}
+                  defaultValue={item.todo.split("|")[0]}
+                />
+                <input
+                  id={`desc${item.id}`}
+                  className={`${
+                    item.completed ? "line-through" : ""
+                  } text-gray-300`}
+                  defaultValue={item.todo.split("|")[1]}
+                />
+              </div>
+              <button
+                className="ml-2 rounded bg-blue-500 text-white w-14 hover:bg-blue-800 mr-1"
+                onClick={() => handleEditItem(item.id)}
+              >
+                Update
+              </button>
+              <button
+                className="rounded bg-orange-500 text-white w-14 hover:bg-orange-800"
+                onClick={() => handleDeleteItem(item.id)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        )
+      })
+    )
+  }
+  const handleAddItem = () => {
+    fetch(urlAddTodo, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        todo: `${inputTitle.current.value}|${inputDesc.current.value}`,
+        completed: false,
+        userId: fakeUserId
+      })
+    })
+      .then((res) => res.json())
+      .then((todo) => {
+        // need this variable to make saving to localStorage exactly because setCache() is async
+        const newdata = [
+          ...cache,
+          { ...todo, id: Math.floor(Math.random() * 1000) }
+        ]
+
+        setCache(newdata)
+        localStorage.setItem("todos", JSON.stringify(newdata))
+        inputTitle.current.value = ""
+        inputDesc.current.value = ""
+        setIsDisabled(true)
+      })
+  }
+  const handleEditItem = (todoId) => {
+    const titleInput = document.getElementById(`title${todoId}`)
+    const descInput = document.getElementById(`desc${todoId}`)
+    const tmpArr = [...cache]
+
+    if (titleInput.value.trim() === "") {
+      titleInput.value = titleInput.defaultValue
+      return
+    }
+
+    if (
+      titleInput.value.trim() !== titleInput.defaultValue ||
+      descInput.value.trim() !== descInput.defaultValue
+    ) {
+      tmpArr[
+        tmpArr.findIndex((e) => e.id == todoId)
+      ].todo = `${titleInput.value}|${descInput.value}`
+      setCache(tmpArr)
+    }
+  }
+  const handleDeleteItem = (todoId) => {
+    const tmpArr = [...cache]
+    tmpArr.splice(
+      tmpArr.findIndex((e) => e.id == todoId),
+      1
+    )
+    setCache(tmpArr)
+  }
+  const handleChangeCheckbox = (todoId) => {
+    const tmpArr = [...cache]
+    const i = tmpArr.findIndex((e) => e.id == todoId)
+    tmpArr[i].completed = !tmpArr[i].completed
+    setCache(tmpArr)
+  }
+
+  const checkValidAdd = (ev) => {
+    if (ev.target.value.trim() === "") {
+      setIsDisabled(true)
+      ev.target.classList.add("border-[#ff0000]")
+    } else {
+      setIsDisabled(false)
+      ev.target.classList.remove("border-[#ff0000]")
+    }
+  }
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <h1 className="text-2xl font-semibold mb-4">Wow Todo List</h1>
+      <div className="bg-white rounded-lg shadow p-4">
+        <div className="flex flex-col gap-x-2">
+          <input
+            type="text"
+            placeholder="Title"
+            className="w-full border rounded p-2 mb-2"
+            ref={inputTitle}
+            onBlur={checkValidAdd}
+          />
+          <input
+            type="text"
+            placeholder="Description"
+            className="w-full border rounded p-2 mb-2"
+            ref={inputDesc}
+          />
+          <button
+            className={`rounded w-24 mb-2 text-white  ${
+              isDisabled
+                ? "bg-gray-500 cursor-not-allowed focus:outline-none disabled:opacity-75"
+                : "bg-blue-500 hover:bg-blue-800"
+            }`}
+            onClick={handleAddItem}
+            disabled={isDisabled}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Add
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="bg-white rounded-lg shadow p-4 mt-5">
+        <ul className="space-y-2">{todoList}</ul>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </>
+  )
 }
